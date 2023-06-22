b5da2de9399390fd3a8f25d8c3a30158c4330fc2
unknown <bhansalimadhav03@gmail.com>
2023-06-23 04:30:37 +0530

    init commit

---
+++ client/src/scenes/performance/index.jsx
@@ -0,0 +1,92 @@
+import React from "react";
+import Header from "components/Header";
+import { Box, useTheme } from "@mui/material";
+import { useGetUserPerformanceQuery } from "state/api";
+import { DataGrid } from "@mui/x-data-grid";
+import CustomColumnMenu from "components/DataGridCustomColumnMenu";
+import { useSelector } from "react-redux";
+
+function Performance() {
+  const userId = useSelector((state) => state.global.userId);
+  const { data, isLoading } = useGetUserPerformanceQuery(userId);
+  const theme = useTheme();
+  // console.log("data", data);
+  const columns = [
+    {
+      field: "_id",
+      headerName: "ID",
+      flex: 1,
+    },
+    {
+      field: "userId",
+      headerName: "User ID",
+      flex: 1,
+    },
+    {
+      field: "createdAt",
+      headerName: "Created At",
+      flex: 1,
+    },
+    {
+      field: "products",
+      headerName: "# of Products",
+      flex: 0.5,
+      sortable: true,
+      renderCell: (params) => params.value.length,
+    },
+    {
+      field: "cost",
+      headerName: "Cost",
+      flex: 1,
+      renderCell: (params) => `$${Number(params.value).toFixed(2)}`,
+    },
+  ];
+  return (
+    <Box m={"1.5rem 2.5rem"}>
+      <Header
+        title={"PERFORMANCE"}
+        subtitle={"Track your Affiliate Sales Performance Here"}
+      />
+      <Box
+        height={"74vh"}
+        marginTop={"40px"}
+        sx={{
+          "& .MuiDataGrid-root": {
+            border: "none",
+          },
+          "& .MuiDataGrid-cell": {
+            borderBottom: "none",
+          },
+          "& .MuiDataGrid-columnHeaders": {
+            backgroundColor: theme.palette.background.alt,
+            color: theme.palette.secondary[100],
+            borderBottom: "none",
+          },
+          "& .MuiDataGrid-virtualScroller": {
+            backgroundColor: theme.palette.primary.light,
+          },
+          "& .MuiDataGrid-footerContainer": {
+            backgroundColor: theme.palette.background.alt,
+            color: theme.palette.secondary[100],
+            borderTop: "none",
+          },
+          "& .MuiDataGrid-toolbarContainer .MuiButton-text": {
+            color: `${theme.palette.secondary[200]} !important`,
+          },
+        }}
+      >
+        <DataGrid
+          loading={isLoading || !data}
+          columns={columns}
+          rows={(data && data.sales) || []}
+          getRowId={(row) => row._id}
+          components={{
+            ColumnMenu: CustomColumnMenu,
+          }}
+        />
+      </Box>
+    </Box>
+  );
+}
+
+export default Performance;
